2f632bba9cccb6b2d1d34d8c8403bf8ad04e0c2a
sakkke <84666033+sakkke@users.noreply.github.com>
2024-04-07 08:11:54 +0900

    build: add Git versioning logic

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,9 +1,41 @@
+import org.eclipse.jgit.api.Git
+import org.eclipse.jgit.storage.file.FileRepositoryBuilder
+
 plugins {
     kotlin("jvm") version "1.9.22"
 }
 
 group = "dev.sakkke"
-version = "0.1.0-SNAPSHOT"
+//version = "0.1.0-SNAPSHOT"
+
+buildscript {
+    repositories {
+        mavenCentral()
+    }
+    dependencies {
+        // https://mvnrepository.com/artifact/org.eclipse.jgit/org.eclipse.jgit
+        classpath("org.eclipse.jgit:org.eclipse.jgit:6.9.0.202403050737-r")
+    }
+}
+
+val setVersionFromGit = tasks.register("setVersionFromGit") {
+    doLast {
+        val repo = FileRepositoryBuilder.create(File(".git"))
+        val git = Git(repo)
+        val describe = git.describe().setTags(true).call()
+        if (describe != null) {
+            val versionWithoutV = describe.removePrefix("v")
+            project.version = versionWithoutV
+        } else {
+            project.version = "0.1.0-SNAPSHOT"
+        }
+        println("Project version set to: ${project.version}")
+    }
+}
+
+tasks.named("compileKotlin") {
+    dependsOn(setVersionFromGit)
+}
 
 repositories {
     mavenCentral()
@@ -22,10 +54,7 @@ tasks.test {
 }
 
 tasks.jar {
-    from({
-        configurations.runtimeClasspath.get().filter { it.isDirectory }.map { it }
-        configurations.runtimeClasspath.get().filter { !it.isDirectory }.map { zipTree(it) }
-    })
+    from(configurations.runtimeClasspath.get().map { if (it.isDirectory) it else zipTree(it) })
 }
 kotlin {
     jvmToolchain(17)
